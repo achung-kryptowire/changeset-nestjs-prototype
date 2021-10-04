45556ce8a5d046ab2d1cefa6f78a36b755ca6f6e
Andrew Chung <achung@kryptowire.com>
2021-10-04 11:30:24 -0400

    bleh

--- scripts/pre-release/publish.mjs
+++ scripts/pre-release/publish.mjs
@@ -1,3 +1,4 @@
 #!/usr/bin/env zx
 
+console.log(`========= Publishing repository ==========`);
 await $`npm_config_registry=https://krwr.jfrog.io/artifactory/api/npm/main/ npm exec -- changeset publish`;
--- scripts/pre-release/version.mjs
+++ scripts/pre-release/version.mjs
@@ -4,8 +4,10 @@ const preFilePath = path.join(__dirname, '../../.changeset/pre.json');
 if (await fs.pathExists(preFilePath)) {
   const pre = JSON.parse((await fs.readFile(preFilePath)).toString());
   if (pre.mode !== 'pre') {
+    console.log(`============== Entering pre mode ================`);
     await $`npm exec -- changeset pre enter beta`;
   }
 }
 
+console.log('================== Versioning ==============');
 await $`npm exec -- changeset version`;
