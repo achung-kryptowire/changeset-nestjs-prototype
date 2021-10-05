4717f74c669a07def49056bb83f53ad30772a311
Andrew Chung <achung@kryptowire.com>
2021-10-05 09:47:26 -0400

    blehy

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -13,7 +13,7 @@ describe('AppController', () => {
 
     appController = app.get<AppController>(AppController);
   });
-
+  // hihih
   describe('root', () => {
     it('should return "Hello World!"', () => {
       expect(appController.getHello()).toBe('Hello World!');
